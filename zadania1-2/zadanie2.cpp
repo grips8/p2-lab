e20e67a5a810d47f813dae28913696ea9c33ae71
grips8 <grypowski@gmail.com>
2021-03-12 18:11:58 +0100

    Poprawka na zadanie2 - dodanie klasycznego wskaznika

--- zadania1-2/zadanie2.cpp
+++ zadania1-2/zadanie2.cpp
@@ -1,6 +1,12 @@
 // zadanie 2
 #include <memory>
 
+struct MeleeWeapon
+{
+    std::string name = "bagnet francuski";
+    std::string filler = "llorem ipsum ..";
+};
+
 struct Weapon
 {
     std::string model = "HK416";
@@ -14,6 +20,7 @@ struct Soldier
     std::string surname = "Smith";
     std::string nationality = "USA";
     std::shared_ptr<Weapon> basicWeapon;
+    MeleeWeapon melee;
 };
 
 void Implementation ()
@@ -26,6 +33,11 @@ void Implementation ()
     std::shared_ptr<Weapon> weapon_1(new Weapon);
     std::shared_ptr<Weapon> weapon_2(new Weapon);
     std::shared_ptr<Weapon> weapon_3(new Weapon);
+
+    MeleeWeapon* melee1 = new MeleeWeapon;
+    MeleeWeapon* melee2 = new MeleeWeapon;
+
+    (*melee2).name = "maczeta krakowska";
 
 
     weapon_2->model = "AN-94";
@@ -41,19 +53,27 @@ void Implementation ()
     soldier_2->surname = "Kimmich";
     soldier_2->nationality = "Germany";
     soldier_2->basicWeapon = weapon_1;
+    soldier_2->melee = *melee1;
 
     soldier_3->name = "Anatoly";
     soldier_3->surname = "Karpov";
     soldier_2->nationality = "Russia";
     soldier_3->basicWeapon = weapon_2;
+    soldier_3->melee = *melee2;
 
     soldier_4->name = "Logan";
     soldier_4->surname = "Thomson";
     soldier_2->nationality = "Scotland";
     soldier_4->basicWeapon = weapon_3;
+    soldier_4->melee = * melee2;
 
     (*soldier_1).basicWeapon = weapon_3;
+    (*soldier_1).melee = *melee1;
+    (*soldier_1).melee.name = "scyzoryk";
     (*(*soldier_1).basicWeapon).model = "L86";
+
+    std::cout << soldier_1->melee.name << std::endl;
+    std::cout << (*soldier_2).melee.name << std::endl;
 
     std::cout << soldier_1->basicWeapon->model << std::endl;
     std::cout << soldier_4->basicWeapon->model << std::endl;
@@ -62,4 +82,7 @@ void Implementation ()
     delete soldier_2;
     delete soldier_3;
     delete soldier_4;
+
+    delete melee1;
+    delete melee2;
 }
